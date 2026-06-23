138241e0e614e7824fffc96947bced540f9f0392
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2026-06-23 12:18:26 +0200

    Add files via upload

--- utils/syncBela.m
+++ utils/syncBela.m
@@ -1,27 +1,30 @@
-function syncBela(savepath, project_name, ip_address)
+function syncBela(savepath, project_name, ip_address, deleteAfterSync)
 % syncBela saves audio files from Bela device to the local machine.
-% ![IMPORTANT] Make sure the Bela decice is conencted via USB before
-% running the script. 
+% Optionally deletes the files from Bela after successful synchronization.
 %
-%   Inputs:
-%       savepath     - Local directory where files will be saved (string/char)
-%       project_name - Name of the Bela project folder on the device (string/char)
-%       ip_address   - IP address of the Bela device (string/char)
+% Inputs:
+%   savepath        - Local directory where files will be saved
+%   project_name    - Name of the Bela project folder
+%   ip_address      - IP address of the Bela device
+%   deleteAfterSync - (optional) true/false. If true, files are deleted
+%                     from Bela after syncing. Default = false.
 %
-%   The function retrieves files matching:
-%       - audiofile_*
-%       - audio_features_*
-%
-%   Example:
-%       syncBela(savepath, project_name, ip_address);
+% Example:
+%   syncBela(savepath, project_name, ip_address)
+%   syncBela(savepath, project_name, ip_address, true)
+
+    % default value
+    if nargin < 4
+        deleteAfterSync = false;
+    end
 
     % create local directory if not exist
     if ~exist(savepath,'dir')
         mkdir(savepath);
     end
-    
+
     % define file patterns to be copied from the Bela device
-    files2load = { 'audiofile_*','audio_features_*' };
+    files2load = {'audiofile_*', 'audio_features_*'};
 
     % testing SSH connection to the Bela device
     testCmd = sprintf('ssh root@%s "echo connection_ok"', ip_address);
@@ -29,13 +32,35 @@ function syncBela(savepath, project_name, ip_address)
 
     % checking connection status
     if status ~= 0
-        error('Cannot connect to Bela at %s. Please make sure Bela is connected to the computer via USB.', ip_address);
-    else    
-        % if connected loop through files and save them 
-        for k = 1:length(files2load)
-            cmd = sprintf(['scp root@', ip_address, ':/root/Bela/projects/', project_name, '/%s "%s"'], files2load{k}, savepath);
-            system(cmd);
+        error(['Cannot connect to Bela at %s. Please make sure Bela ' ...
+               'is connected to the computer via USB.'], ip_address);
+    end
+
+    % sync files
+    for k = 1:length(files2load)
+
+        % copy files
+        copyCmd = sprintf(['scp root@%s:/root/Bela/projects/%s/%s "%s"'], ...
+                          ip_address, project_name, files2load{k}, savepath);
+
+        status = system(copyCmd);
+
+        % optionally delete after successful copy
+        if deleteAfterSync && status == 0
+            deleteCmd = sprintf(['ssh root@%s "rm -f ' ...
+                                 '/root/Bela/projects/%s/%s"'], ...
+                                 ip_address, project_name, files2load{k});
+            system(deleteCmd);
         end
-    end 
+    end
+    
     dir(savepath)
-end 
+
+    fprintf('\nSync completed.\n');
+
+    if deleteAfterSync
+        fprintf('Source files deleted from Bela.\n');
+    end
+
+    
+end
